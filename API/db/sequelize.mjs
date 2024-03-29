9984abc7524411eb79c67b2b475f7175c60edd78
LucasSimoesPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-29 14:36:45 +0100

    Import the user
    It has test values

--- API/db/sequelize.mjs
+++ API/db/sequelize.mjs
@@ -1,5 +1,7 @@
 import { Sequelize, DataTypes } from "sequelize";
 import bcrypt from "bcrypt"
+import { userModel } from "../models/db_legendsAnalyzer.mjs";
+import 'dotenv/config'
 
 // Establish connection to the MySQL database using sequelize
 const sequelize = new Sequelize(
@@ -14,3 +16,29 @@ const sequelize = new Sequelize(
     }
 )
 
+const User = userModel(sequelize, DataTypes)
+
+let initDb = () => {
+    return sequelize
+            .sync({force: true})
+            .then((_) => {
+                importUser();
+                console.log("The database was synced")
+            })
+}
+
+const importUser = () => {
+    bcrypt
+        .hash(process.env.IMPORT_USER_PASS, 50)
+        .then((hash) => {
+            User.create({
+                id_user,
+                nickname: process.env.IMPORT_USER_NICKNAME,
+                email: process.env.IMPORT_USER_EMAIL,
+                password: hash
+            }) 
+        })
+}
+
+
+export { initDb }
