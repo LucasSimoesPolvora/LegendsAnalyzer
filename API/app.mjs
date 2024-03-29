57bcbb22ecba97f64b9e9923ff00d0ef2d1a5d8b
LucasSimoesPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-29 14:37:10 +0100

    Fix a bug about the dotenv
    Co-Authored-By: ReactedTitan880 <122774874+ThomNardou@users.noreply.github.com>

--- API/app.mjs
+++ API/app.mjs
@@ -1,9 +1,12 @@
 import express from 'express';
+import { initDb } from './db/sequelize.mjs';
+import 'dotenv/config'
 
 const app = express()
 app.use(express.json())
 const port = 3000
 
+initDb()
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}/`)
