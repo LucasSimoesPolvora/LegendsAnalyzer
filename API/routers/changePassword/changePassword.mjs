ab61b411936a2adca8526ddb48a3ebf5a5b4b47c
LucasSimoesPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-04-02 16:06:50 +0200

    Hassoul Add a condition
    The condition obliges the user to have a password with the 10 caracters. It makes a sort of new protection to get the 10 caracters

--- API/routers/changePassword/changePassword.mjs
+++ API/routers/changePassword/changePassword.mjs
@@ -1,37 +1,43 @@
-import express from "express"
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-import { User } from "../../db/sequelize.mjs"
-import { privateKey } from "../../auth/private_key.mjs"
-import { auth } from "../../auth/auth.mjs"
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { User } from "../../db/sequelize.mjs";
+import { privateKey } from "../../auth/private_key.mjs";
+import { auth } from "../../auth/auth.mjs";
 
 // Creating the router
 const changePasswordRouter = express();
 
-changePasswordRouter.put('/:id', auth, (req, res) => {
-    const userId = req.params.id;
+changePasswordRouter.put("/:id", auth, (req, res) => {
+  const userId = req.params.id;
 
+  //To make sure that the password is 10 caracters long
+  if (req.body.password.length >= process.env.PASS_MIN_LENGTH) {
     bcrypt.hash(req.body.password, 10).then((hash) => {
-        User.update({password: hash},  {where: {id_user: userId}})
-            .then((_) => {
-                // Finding user by primary key
-                return User.findByPk(userId)
-                            .then((updatedUser) => {
-                                // If it doesn't exist, return code 404
-                                if(updatedUser === null){
-                                    const message = "The user requested doesn't exist. Try again later"
-                                    res.status(404).json({ message })
-                                }
-                                // If the user is updated successfuly. return code 200
-                                const message = `The user ${updatedUser.nickname} was successfuly updated`
-                                res.status(200).json({ message })
-                            })
-            })
-            .catch((error) => {
-                const message = "The user couldn't be updated. Try again later"
-                res.status(500).json({ message, data: error })
-            })
-    })
-})
+      User.update({ password: hash }, { where: { id_user: userId } })
+        .then((_) => {
+          // Finding user by primary key
+          return User.findByPk(userId).then((updatedUser) => {
+            // If it doesn't exist, return code 404
+            if (updatedUser === null) {
+              const message =
+                "The user requested doesn't exist. Try again later";
+              res.status(404).json({ message });
+            }
+            // If the user is updated successfuly. return code 200
+            const message = `The user ${updatedUser.nickname} was successfuly updated`;
+            res.status(200).json({ message });
+          });
+        })
+        .catch((error) => {
+          const message = "The user couldn't be updated. Try again later";
+          res.status(500).json({ message, data: error });
+        });
+    });
+  } else {
+    const message = "The password length doesn't match the requirements"
+    res.status(401).json({ message })
+  }
+});
 
-export { changePasswordRouter }
+export { changePasswordRouter };
--- API/routers/signUp/signup.mjs
+++ API/routers/signUp/signup.mjs
@@ -10,30 +10,35 @@ const signUpRouter = express();
 
 // endpoint for handling the router
 signUpRouter.post("/", (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    User.create({
-        nickname: req.body.nickname,
-        email: req.body.email,
-        password: hash
-    })
-      .then((createdUser) => {
-        // Return success message upon successful creation
-        const message = `Your account has been successfully created !`;
-        res.status(200).json({ message });
+  if(req.body.password.length >= process.env.PASS_MIN_LENGTH){
+    bcrypt.hash(req.body.password, 10).then((hash) => {
+      User.create({
+          nickname: req.body.nickname,
+          email: req.body.email,
+          password: hash
       })
-      .catch((error) => {
-        // If the error is a validation error, return a 400 status code with the error message
-        if (error instanceof ValidationError) {
-          return res.status(400).json({
-            message: error.message,
-            data: error,
-          });
-        }
-        // If there is any other error during the process
-        const message = "The account couldn't be created. Try again later";
-        res.status(500).json({ message, data: error });
-      });
-  });
+        .then((createdUser) => {
+          // Return success message upon successful creation
+          const message = `Your account has been successfully created !`;
+          res.status(200).json({ message });
+        })
+        .catch((error) => {
+          // If the error is a validation error, return a 400 status code with the error message
+          if (error instanceof ValidationError) {
+            return res.status(400).json({
+              message: error.message,
+              data: error,
+            });
+          }
+          // If there is any other error during the process
+          const message = "The account couldn't be created. Try again later";
+          res.status(500).json({ message, data: error });
+        });
+    });
+  } else {
+    const message = "The password length doesn't match the requirements"
+    res.status(401).json({ message })
+  }
 });
 
 export { signUpRouter };
