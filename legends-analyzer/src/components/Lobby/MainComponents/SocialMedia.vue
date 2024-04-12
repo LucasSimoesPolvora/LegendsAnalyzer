d8d5e27f7ffd8ae185706b8cd6021e7ca52db951
ReactedTitan880 <thomasnardou@icloud.com>
2024-04-12 11:51:39 +0200

    hassoul finish the social media part

--- legends-analyzer/src/components/Lobby/MainComponents/SocialMedia.vue
+++ legends-analyzer/src/components/Lobby/MainComponents/SocialMedia.vue
@@ -4,22 +4,32 @@
       <h1>Our social networks</h1>
       <p>Join Us</p>
     </div>
-    <div class="socialCard" :style="{backgroundColor: 'red'}">
-        <AkDiscordFill class="icon"/>
+    <div class="cardDisplay">
+      <div class="socialCard" :style="{ backgroundColor: '#7289da' }">
+        <AkDiscordFill class="icon" />
         <h3>Discord</h3>
+      </div>
+      <div class="socialCard" :style="{ backgroundColor: '#C13584' }">
+        <AkInstagramFill class="icon" />
+        <h3>Instagram</h3>
+      </div>
+      <div class="socialCard" :style="{ backgroundColor: '#1DA1F2' }">
+        <AkTwitterFill class="icon" />
+        <h3>Twitter (X)</h3>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { AkDiscordFill } from "@kalimahapps/vue-icons";
+import { AkDiscordFill, AkInstagramFill, AkTwitterFill } from '@kalimahapps/vue-icons'
 </script>
 
 <script>
 export default {}
 </script>
 <style>
-.socialMediaPart .socialTitles{
+.socialMediaPart {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -28,17 +38,32 @@ export default {}
   color: white;
 }
 
-.socialCard {
-    width: 20vh;
-    height: 100px;
+.socialTitles {
+  text-align: center;
+  width: 50vh;
+  padding-bottom: calc(10vh - 2px);
+  border-bottom: 2px solid white;
 }
 
-.socialCard img {
-    width: 20%;
-    object-fit: cover;
+.socialCard {
+  width: 20vh;
+  height: 100px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
 }
 
 .icon {
   font-size: 2.3em;
+}
+
+.cardDisplay {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  margin-top: 10vh;
+  /* height: 50vh; */
+  /* border-top: 2px solid white; */
 }
 </style>
