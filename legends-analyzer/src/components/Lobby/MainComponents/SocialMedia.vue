7f8feb035b7dd094dba40d94454e53bb2bff9878
ReactedTitan880 <thomasnardou@icloud.com>
2024-04-13 15:31:45 +0200

    Update SocialMedia.vue

--- legends-analyzer/src/components/Lobby/MainComponents/SocialMedia.vue
+++ legends-analyzer/src/components/Lobby/MainComponents/SocialMedia.vue
@@ -5,18 +5,24 @@
       <p>Join Us</p>
     </div>
     <div class="cardDisplay">
-      <div class="socialCard" :style="{ backgroundColor: '#7289da' }">
-        <AkDiscordFill class="icon" />
-        <h3>Discord</h3>
-      </div>
-      <div class="socialCard" :style="{ backgroundColor: '#C13584' }">
-        <AkInstagramFill class="icon" />
-        <h3>Instagram</h3>
-      </div>
-      <div class="socialCard" :style="{ backgroundColor: '#1DA1F2' }">
-        <AkTwitterFill class="icon" />
-        <h3>Twitter (X)</h3>
-      </div>
+      <a href="">
+        <div class="socialCard" :style="{ backgroundColor: '#7289da' }">
+          <AkDiscordFill class="icon" />
+          <h3>Discord</h3>
+        </div>
+      </a>
+      <a href="">
+        <div class="socialCard" :style="{ backgroundColor: '#C13584' }">
+          <AkInstagramFill class="icon" />
+          <h3>Instagram</h3>
+        </div>
+      </a>
+      <a href="">
+        <div class="socialCard" :style="{ backgroundColor: '#1DA1F2' }">
+          <AkTwitterFill class="icon" />
+          <h3>Twitter (X)</h3>
+        </div>
+      </a>
     </div>
   </div>
 </template>
@@ -31,18 +37,21 @@ export default {}
 <style>
 .socialMediaPart {
   display: flex;
-  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   height: 50vh;
+  width: 100%;
   color: white;
 }
 
 .socialTitles {
   text-align: center;
-  width: 50vh;
-  padding-bottom: calc(10vh - 2px);
-  border-bottom: 2px solid white;
+  width: 50%;
+  height: 30vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-right: 2px solid white;
 }
 
 .socialCard {
@@ -53,17 +62,22 @@ export default {}
   align-items: center;
 }
 
-.icon {
-  font-size: 2.3em;
+.cardDisplay {
+  width: 50%;
+  display: grid;
+  justify-content: center;
 }
 
-.cardDisplay {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  width: 100%;
-  margin-top: 10vh;
-  /* height: 50vh; */
-  /* border-top: 2px solid white; */
+.cardDisplay a {
+  text-decoration: none;
+  color: white;
+}
+
+.cardDisplay .socialCard {
+  margin: 10px 0;
+}
+
+.icon {
+  font-size: 2.3em;
 }
 </style>
