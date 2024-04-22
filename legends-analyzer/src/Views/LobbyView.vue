52a57c98bb9269d3de80c93c2ee6997a68e5ccae
ReactedTitan880 <thomasnardou@icloud.com>
2024-04-22 22:32:13 +0200

    Hassoul Update LobbyView.vue

--- legends-analyzer/src/Views/LobbyView.vue
+++ legends-analyzer/src/Views/LobbyView.vue
@@ -5,11 +5,15 @@ import MainComponent from '@/components/Lobby/Main.vue'
 
 <template>
   <HeaderComponent />
-  <MainComponent />
+  <MainComponent id="main"/>
 </template>
 
 <style>
 main {
   background-color: black;
+}
+
+#main {
+  box-shadow: 0px 0px 30px 60px #000000;
 }
 </style>
