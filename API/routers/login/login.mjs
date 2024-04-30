8741b852d7b2cc5be8e3c1466264f764ca3b7c11
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-04-30 16:30:44 +0200

    Hassoul use cookie to get the value of the cookie server-side

--- API/routers/login/login.mjs
+++ API/routers/login/login.mjs
@@ -1,50 +1,60 @@
-import express from "express"
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-import { User } from "../../db/sequelize.mjs"
-import { privateKey } from "../../auth/private_key.mjs"
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { User } from "../../db/sequelize.mjs";
+import { privateKey } from "../../auth/private_key.mjs";
+import cookie from 'cookie'
+
 // Creating the router
 const loginRouter = express();
 
 // endpoint for handling the router
 loginRouter.post("/", (req, res) => {
-    // Finding the user in the database by username
-    User.findOne({ where: { username: req.body.username }})
-        .then((user) => {
-            // If user doesn't exist, return 404 error
-            if (!user) {
-                const message = `Username not found`;
-                return res.status(401).json({ message });
-            }
-            // Comparing the provided password with the hashed password stored in the database
-            bcrypt.compare(req.body.password, user.password)
-                .then((isPasswordValid) => {
-                    // If password is invalid, return 401 error
-                    if (!isPasswordValid) {
-                        const message = `The password is incorrect`
-                        return res.status(401).json({ message });
-                    } else {
-                        // If password is valid, generate JWT token
-                        const token = jwt.sign({ userId: user.id_user}, privateKey, {
-                            expiresIn: "1y" // Token expires in 1 year
-                        });
-                        const message = `The user was successfully connected`;
-                        // Return success message along with user data and token
-                        return res.cookie('Token', token, {
-                            path: '/',
-                            httpOnly: true,
-                            secure: true,
-                            maxAge: 3600000,
+  // Finding the user in the database by username
+  User.findOne({ where: { username: req.body.username } })
+    .then((user) => {
+      // If user doesn't exist, return 404 error
+      if (!user) {
+        const message = `Username not found`;
+        return res.status(401).json({ message });
+      }
+      // Comparing the provided password with the hashed password stored in the database
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((isPasswordValid) => {
+          // If password is invalid, return 401 error
+          if (!isPasswordValid) {
+            const message = `The password is incorrect`;
+            return res.status(401).json({ message });
+          } else {
+            // If password is valid, generate JWT token
+            const token = jwt.sign({ userId: user.id_user }, privateKey, {
+              expiresIn: "1y", // Token expires in 1 year
+            });
+            const message = `The user was successfully connected`;
+            // Return success message along with user data and token
+            res.cookie("jwt", token, {
+              httpOnly: true,
+              secure: false,
+              sameSite: "strict",
+              path: "/",
+            });
+            res.setHeader("Access-Control-Allow-Credentials", "true"); 
+            res.setHeader("Access-Control-Expose-Headers", "Cache-Control");
 
-                        }).status(201).json({ message, data: user, token});
-                    }
-                });
-        })
-        .catch((error) => {
-            // If an error occurs during the process, return a generic error message
-            const message = `The user couldn't be connected. Try again later`;
-            return res.status(500).json({ message, data: error });
-        })
-})
+            let cookieValues = cookie.parse(req.headers.cookie)
+
+            console.log(cookieValues)
+
+            res.status(201).json({ message, data: user });
+          }
+        });
+    })
+    .catch((error) => {
+      // If an error occurs during the process, return a generic error message
+      const message = `The user couldn't be connected. Try again later`;
+      return res.status(500).json({ message, data: error });
+    });
+});
 
 export { loginRouter }; // Exporting the router for use in other files
