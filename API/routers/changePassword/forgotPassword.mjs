4c1d9f238e6ccec15b87889af67f8cc187fae61f
LucasPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-05-08 17:19:34 +0200

    Hassoul add the nodemailer package

--- API/routers/changePassword/forgotPassword.mjs
+++ API/routers/changePassword/forgotPassword.mjs
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../../db/sequelize.mjs";
 import { privateKey } from "../../auth/private_key.mjs";
 import { auth } from "../../auth/auth.mjs";
+import nodemailer from 'nodemailer' 
 
 // Creating the router
 const forgotPasswordRouter = express();
@@ -11,11 +12,17 @@ const forgotPasswordRouter = express();
 forgotPasswordRouter.put("/", (req, res) => {
     User.findOne({ where: { email: req.body.email } })
         .then((user) => {
-            if (user) {
-                res.json({ msg: 'hello' })
-            } else {
-                res.json({ msg: " not working" })
+            if(!user){
+                res.status(200).json({ msg: 'The operation was successful !'})
             }
+            const rdmPassword = Math.random().toString(36).slice(-10);
+            bcrypt.hash(rdmPassword, 10).then((hash) => {
+                User.update({ password: hash }, { where: { id_user: user.id_user } })
+                    .then((user) => {
+                        res.json({user})
+                    })
+            })
+            
         }).catch((error) => {
             res.status(500).json({msg: error})
         })
